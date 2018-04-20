f391445706d9b0aed120d0d77e1ea6331184fc69
codemode9 <timourbasegmez@hotmail.com>
2018-04-20 14:02:32 -0400

    start of Nav comp

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Loginform from "./components/Loginform";
+import Nav from "./components/Nav";
+
+
+
+const game = [{rapper: "common"},{rapper: "nas"}];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav />
       </div>
     );
   }
