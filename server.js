1e5efbab9a6ff6c6d45bda3e291dae48fe91a0bb
Devon Williams <dvnwllms009@gmail.com>
2018-04-24 12:49:49 -0400

    Building for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require('body-parser');
 const morgan = require('morgan');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,6 +14,10 @@ app.use(bodyparser.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
   }
 
 require('./passport')(app);
