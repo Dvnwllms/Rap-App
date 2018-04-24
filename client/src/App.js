aae5499dfa5eebc00ca7330ca27dc786202086c0
Devon Williams <dvnwllms009@gmail.com>
2018-04-24 10:44:52 -0400

    ugh

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div>
       <Nav />
       <Router>
-        <Switch>
+        <Switch> 
         <Route exact path='/' component={LandingPage} />
         <Route exact path='/signup' component={CreateAccountPage} />
         <Route exact path='/login' component={LoginPage} />
